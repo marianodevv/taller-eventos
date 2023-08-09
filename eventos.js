c0fa0e80a3f792f4302a02f2add5e1eb88a40b86
MarianoDevv <141088609+MarianoDevv@users.noreply.github.com>
2023-08-08 23:50:03 -0300

    Forzando error

--- eventos.js
+++ eventos.js
@@ -1,7 +1,5 @@
 let div= document.getElementById("divAlert");
 
-div.addEventListener("click", function(event) {
-    if (event.target===div) {
-        alert("Hola! Soy el div");
-    }
+div.addEventListener("click", function() {
+    alert("Hola! Soy el div");
 });
